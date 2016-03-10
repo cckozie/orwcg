f99d8d156e58f2c7e821300fbde44d07d909f9a0
Chris Kosieracki <cckozie@Chriss-MacBook-Air-3.local>
2016-03-10 15:35:06 -0600

    second3 commit

--- js/index.js
+++ js/index.js
@@ -4,10 +4,3 @@
     function onDeviceReady() {
         console.log('device is ready');
     }
-    window.addEventListener('load', function() {
-        document.body.addEventListener('touchmove', function(e) {
-            console.log('touched');
-            e.preventDefault();
-        }, false);
-    }, false);
-    window.addEventListener("touchstart", function(e) {    e.preventDefault();}, false);window.addEventListener("touchmove", function(e) {    e.preventDefault();}, false);
