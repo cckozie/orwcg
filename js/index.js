4518d4356bb4a5da9ad9aaaf82d64191aedb929a
Chris Kosieracki <cckozie@Chriss-MacBook-Air-3.local>
2016-03-11 15:53:18 -0600

    third10 commit

--- js/index.js
+++ js/index.js
@@ -18,10 +18,10 @@
 
     $(document).ready(function(){
         for(var i=1; i<=2; i++) {
-            var tst = $("#p"+i).text();
+            var tst = $("#p"+i).html();
            console.log(tst);
            if(tst.length < 40) {
-                $("#p"+i).html("<br>"+$("#p"+i).text());
+                $("#p"+i).html("<br>"+tst);
 //                $("#p"+i).css("margin-top", "40px")
 //                console.log($("#pbtn"+i).text());
            }
